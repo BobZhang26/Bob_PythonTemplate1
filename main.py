7071b2f9cebdad5c5128f82c8a354fb88dbed8c0
Bobby Zhang <z.zhang19@imperial.ac.uk>
2023-09-08 13:41:26 -0400

    renew

--- main.py
+++ main.py
@@ -2,17 +2,16 @@
 Main cli or app entry point
 """
 
-from mylib.calculator import add
-import click
+import pandas as pd
+
+# define a function to provide descriptive statistics of a dataset
 
 
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+def pd_descriptive(file):
+    data = pd.read_csv(file)
+    return data.describe()
 
 
-if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+# if __name__ == "__main__":
+# pylint: disable=no-value-for-parameter
+#     add_cli()
