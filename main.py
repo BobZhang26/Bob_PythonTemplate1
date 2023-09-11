cd61524845d28a3f914b2d7fe257ec5cbff98f7b
BobZhang26 <141781876+BobZhang26@users.noreply.github.com>
2023-09-10 22:18:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,6 @@
 Main cli or app entry point
 """
 
-import pandas as pd
-
-# define a function to provide descriptive statistics of a dataset
-
-
-def pd_descriptive(file):
-    data = pd.read_csv(file)
-    return data.describe()
-
 
 # if __name__ == "__main__":
 # pylint: disable=no-value-for-parameter
