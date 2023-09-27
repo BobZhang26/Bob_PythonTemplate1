6c2f57715c5dbdd3405943e092d00a577248869a
BobZhang26 <141781876+BobZhang26@users.noreply.github.com>
2023-09-27 18:20:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 """
 Main cli or app entry point
 """
+def add(x,y):
+  return x + y
 
-
-# if __name__ == "__main__":
-# pylint: disable=no-value-for-parameter
-#     add_cli()
+if __name__ == "__main__":
+  add(1,1)
