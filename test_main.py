39510afdbb9cd4b8664f8fa10ce3b40c7053b2c7
BobZhang26 <141781876+BobZhang26@users.noreply.github.com>
2023-09-27 18:24:55 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,10 +4,10 @@ Test goes here
 import main
 
 def test_func():
-  return add(1,1)
+    return add(1,1)
 
 if __name__ == "__main__":
-  assert test_func() == 2
+    assert test_func() == 2
   
   
 
